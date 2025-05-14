3a846818659a45431aa7fb042b26fbbda1c875c2
Matt-Pron <97278377+Matt-Pron@users.noreply.github.com>
2025-05-14 13:26:40 -0300

    Post OS install configuration

---
+++ matt-install.sh
@@ -0,0 +1,20 @@
+# Full update
+sudo pacman -Syu --noconfirm
+
+# Packages
+sudo pacman -S --needed --noconfirm neovim base-devel git hyprpaper hyprlock hyprpicker waybar rofi-wayland foot yazi lua51 luarocks yarn fastfetch ttf-cascadia-code-nerd github-cli zoxide
+
+# Paru for AUR packages
+git clone https://aur.archlinux.org/paru.git
+cd paru && makepkg -si
+cd .. && rm -rf paru
+
+# AUR Packages
+paru -S --needed --noconfirm hyprshot wlogout wezterm brave-bin simplicity-sddm-theme-git
+
+# Fonts
+git clone https://github.com/g5becks/Cartograph ~/CartographCF
+sudo mv ~/CartographCF /usr/share/fonts/CartographCF
+
+# Removing Unneeded
+sudo pacman -R wofi kitty dolphin
