f14a86dd0f50bb131c7e56bc1de940d3e53607b5
Matt-Pron <97278377+Matt-Pron@users.noreply.github.com>
2025-05-15 15:18:12 -0300

    Update matt-install.sh

--- matt-install.sh
+++ matt-install.sh
@@ -2,7 +2,7 @@
 sudo pacman -Syu --noconfirm
 
 # Packages
-sudo pacman -S --needed --noconfirm neovim base-devel git hyprpaper hyprlock hyprpicker waybar rofi-wayland foot yazi lua51 luarocks yarn fastfetch ttf-cascadia-code-nerd github-cli zoxide
+sudo pacman -S --needed --noconfirm neovim base-devel git hyprpaper hyprlock hyprpicker waybar rofi-wayland foot wezterm yazi lua51 luarocks yarn ripgrep fastfetch ttf-cascadia-code-nerd github-cli zoxide
 
 # Paru for AUR packages
 git clone https://aur.archlinux.org/paru.git
@@ -10,7 +10,7 @@ cd paru && makepkg -si
 cd .. && rm -rf paru
 
 # AUR Packages
-paru -S --needed --noconfirm hyprshot wlogout wezterm brave-bin simplicity-sddm-theme-git
+paru -S --needed --noconfirm hyprshot wlogout brave-bin simplicity-sddm-theme-git
 
 # Fonts
 git clone https://github.com/g5becks/Cartograph ~/CartographCF
